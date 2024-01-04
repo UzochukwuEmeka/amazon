af66ad971f4b7925d2f5f2ea5543a1e048c13239
UzochukwuEmeka <uzochukwuemeka516@gmail.com>
2024-01-04 04:04:26 +0100

    List of Products

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Learning Git now 2.Git works
 2. Git Works
+3. testing typescript
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProduct } from './data'
 
 function App() {
   return (
     <div>
       <header>
         Ts Amazon
-        <main></main>
+        <main>
+          <ul>
+            {sampleProduct.map((product) => (
+              <li key={product.id}>
+                {product.title}
+                <img
+                  src={product.images[0]}
+                  alt="image"
+                  className="product-image"
+                />
+                <p>{product.price}</p>
+              </li>
+            ))}
+          </ul>
+        </main>
         <footer> </footer>
       </header>
     </div>
